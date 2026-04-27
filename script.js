50d9a397b657f0291e91341d17eac8e44e0f6363
kiranafees06-wq <kiranafees06@gmail.com>
2026-04-27 11:46:56 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+function calculate() {
+  let r = document.getElementById("radiusInput").value;
+  let C = 2 * 3.142 * r;
+  let D = 2 * r;
+  let A = 3.142 * r ** 2;
+
+  document.getElementById("C").innerHTML = "Circumference" + C.toFixed(2);
+  document.getElementById("D").innerHTML = "Diameter" + D.toFixed(2);
+  document.getElementById("A").innerHTML = "Area" + A.toFixed(2);
+
+  document.getElementById("ans").style.display = "block";
+
+  if (isNaN(r) || r <= 0){
+    alert("Enter a valid radius");
+    return;
+  }
+}
+
+function reset() {
+  document.getElementById("radiusInput").value = "";
+  document.getElementById("ans").style.display = "none";
+}
